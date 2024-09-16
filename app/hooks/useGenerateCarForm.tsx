e00c7ecbaa0af295bda36ba934457937a1d08a66
Anderson Ferreira <andersonf0018@gmail.com>
2024-09-16 18:23:02 -0300

    fix image order

--- app/hooks/useGenerateCarForm.tsx
+++ app/hooks/useGenerateCarForm.tsx
@@ -1,29 +1,29 @@
-import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing';
-import { Buffer } from 'buffer';
-import { Image } from 'react-native';
-import PizZip from 'pizzip';
-import Docxtemplater from 'docxtemplater';
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import { Buffer } from "buffer";
+import { Image } from "react-native";
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
 
-import { base64CarCertificate } from '../utils/base64-car-certificate.js';
-import dateConverter from '../utils/dateConverter';
+import { base64CarCertificate } from "../utils/base64-car-certificate.js";
+import dateConverter from "../utils/dateConverter";
 import {
   base64DataURLToArrayBuffer,
   arrayBufferToBase64,
-  getResizedDimensions
-} from '../utils/base64-to-doc.js';
-import useDownloadDocx from './useDownloadDocx';
-import { useConverterDocxToPdf } from './useConverterDocxToPdf';
+  getResizedDimensions,
+} from "../utils/base64-to-doc.js";
+import useDownloadDocx from "./useDownloadDocx";
+import { useConverterDocxToPdf } from "./useConverterDocxToPdf";
 
 const useGenerateCarForm = () => {
   const { save } = useDownloadDocx();
   const { convertDocxToPdf } = useConverterDocxToPdf();
   const generateForm = async (data) => {
-    const ImageModule = require('docxtemplater-image-module-free');
-    const isOvacao = data.activity === 'Ovação';
-    const isDesova = data.activity === 'Desova';
-    const isComAvaria = data.avaria === 'Com avarias';
-    const isSemAvaria = data.avaria === 'Sem avarias';
+    const ImageModule = require("docxtemplater-image-module-free");
+    const isOvacao = data.activity === "Ovação";
+    const isDesova = data.activity === "Desova";
+    const isComAvaria = data.avaria === "Com avarias";
+    const isSemAvaria = data.avaria === "Sem avarias";
 
     const formData = {
       containerNumber: data.containerNumber,
@@ -32,13 +32,13 @@ const useGenerateCarForm = () => {
       isDesova: isDesova,
       isComAvaria: isComAvaria,
       isSemAvaria: isSemAvaria,
-      ...data
+      ...data,
     };
 
     const fileName = `Car_Certificate_Nº_${formData.containerNumber}.docx`;
 
     try {
-      const zip = new PizZip(Buffer.from(base64CarCertificate, 'base64'));
+      const zip = new PizZip(Buffer.from(base64CarCertificate, "base64"));
 
       const imageOpts = {
         getImage(tag) {
@@ -65,56 +65,41 @@ const useGenerateCarForm = () => {
               }
             );
           });
-        }
+        },
       };
 
       const doc = new Docxtemplater(zip, {
         paragraphLoop: true,
         linebreaks: true,
-        modules: [new ImageModule(imageOpts)]
+        modules: [new ImageModule(imageOpts)],
       });
-
-      const getInterleavedOrder = (items) => {
-        const result = [];
-        const length = items.length;
-        const middle = Math.ceil(length / 2);
-
-        for (let i = 0; i < middle; i++) {
-          if (i < middle) result.push(items[i]); // Primeira coluna
-          if (i + middle < length) result.push(items[i + middle]); // Segunda coluna
-        }
-
-        return result;
-      };
-
-      // Aplicando a nova ordem intercalada
-      const interleavedPhotos = getInterleavedOrder(formData.image);
-
-      // Atualizando com a numeração sequencial correta
-      const updatedPhotos = interleavedPhotos.map((photo, index) => ({
-        ...photo,
-        imageIndex: `Photo ${index + 1}`
-      }));
 
       return doc
         .resolveData({
           ...formData,
-          image: updatedPhotos
+          image: formData.image.map(
+            ({ imageTitle, imageDescription, imageUrl }, index) => ({
+              imageTitle: imageTitle,
+              imageDescription: imageDescription,
+              imageUrl: imageUrl,
+              imageIndex: `Photo ${index + 1}`,
+            })
+          ),
         })
         .then(async () => {
           doc.render();
           const out = doc.getZip().generate({
-            type: 'base64',
+            type: "base64",
             mimeType:
-              'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
           });
           return {
             base64: out,
-            fileName: fileName
+            fileName: fileName,
           };
         });
     } catch (error) {
-      console.error('Erro ao gerar ou salvar o documento:', error);
+      console.error("Erro ao gerar ou salvar o documento:", error);
     }
   };
 
@@ -123,10 +108,10 @@ const useGenerateCarForm = () => {
     // await save(form.base64, form.fileName, 'docx');
     const doxcData = form.base64;
     const doxcName = `${FileSystem.documentDirectory}${
-      form.fileName.split('.')[0]
+      form.fileName.split(".")[0]
     }.docx`;
     await FileSystem.writeAsStringAsync(doxcName, doxcData, {
-      encoding: FileSystem.EncodingType.Base64
+      encoding: FileSystem.EncodingType.Base64,
     });
     Sharing.shareAsync(doxcName);
 
@@ -137,11 +122,11 @@ const useGenerateCarForm = () => {
     const form: any = await generateForm(data);
     const docxData = form.base64;
     const pdfName = `${FileSystem.documentDirectory}${
-      form.fileName.split('.')[0]
+      form.fileName.split(".")[0]
     }.pdf`;
     const pdfBase64 = await convertDocxToPdf(docxData);
     await FileSystem.writeAsStringAsync(pdfName, pdfBase64, {
-      encoding: FileSystem.EncodingType.Base64
+      encoding: FileSystem.EncodingType.Base64,
     });
 
     Sharing.shareAsync(pdfName);
@@ -152,14 +137,14 @@ const useGenerateCarForm = () => {
   const downloadPdf = async (data) => {
     const form: any = await generateForm(data);
     const docxData = form.base64;
-    const pdfName = `${form.fileName.split('.')[0]}.pdf`;
+    const pdfName = `${form.fileName.split(".")[0]}.pdf`;
     const pdfBase64 = await convertDocxToPdf(docxData);
-    await save(pdfBase64, pdfName, 'pdf');
+    await save(pdfBase64, pdfName, "pdf");
   };
 
   const downloadDocx = async (data) => {
     const form: any = await generateForm(data);
-    await save(form.base64, form.fileName, 'docx');
+    await save(form.base64, form.fileName, "docx");
 
     console.log(`Documento gerado e salvo: ${form.fileName}`);
   };
